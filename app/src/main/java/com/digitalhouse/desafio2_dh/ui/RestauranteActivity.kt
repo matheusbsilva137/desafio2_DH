5431bbddc813a4595e0238beadf23c884428db0a
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2020-11-13 22:57:02 -0300

    Tela Principal - Restaurantes

--- app/src/main/java/com/digitalhouse/desafio2_dh/ui/RestauranteActivity.kt
+++ app/src/main/java/com/digitalhouse/desafio2_dh/ui/RestauranteActivity.kt
@@ -8,6 +8,5 @@ class RestauranteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurante)
-
     }
 }
